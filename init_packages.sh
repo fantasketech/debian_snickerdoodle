0355cb5b16ecdda4a35a1f33023ab566002709e1
nfantaske <fantaske@gmail.com>
2020-09-10 20:21:25 -0700

    work in progress

--- init_packages.sh
+++ init_packages.sh
@@ -1,6 +1,11 @@
-mount -t proc proc /proc
+#!/bin/bash
 
-/var/lib/dpkg/info/dash.preinst install
+[ -f /var/lib/dpkg/info/dash.preinst ] && /var/lib/dpkg/info/dash.preinst install
 
 PATH=/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin  dpkg --configure -a
 
+useradd -m -p "sa3tHJ3/KuYvI" user
+cp /zybot/* /home/user/
+chown user /home/user/*
+rm -r /zybot/
+chmod u+s /usr/bin/sudo
--- make_debian.sh
+++ make_debian.sh
@@ -1,53 +1,81 @@
 #!/bin/bash
 
-kern_path=/home/nf/git/linux_xilinx
+kern_path=/home/nf/git/linux-xlnx
 deb_root=$PWD/root/
-sd_card=/dev/disk/by-id/usb-Generic_MassStorageClass_000000001538-0:1-part1
-sd_mount=/home/nf/sdcard
 
+cp_err_flag=/tmp/zdeb_cp_err
+create_done=/tmp/multistrap_done
+
+image=snickerdoodle.img
+sandbox=/home/nf/git/zynq_box/sandbox
 release="jesse"
 
 [ "$1_" = "buster_" ] && {
 	release="buster"
 }
 
+cp_err(){
+	touch $cp_err_flag
+}
+
 punt(){
 	echo "Exiting early"
+	rm -rf $cp_err_flag
+	rm -rf $create_done
+	[ -z "$(ls -A $deb_root)" ] || {
+		sudo umount $deb_root
+	}
+	rm -rf $image
 	exit
 }
 
-echo "$deb_root"
-echo "$kern_path"
+echo "rootfs: $deb_root"
+echo "kernel source: $kern_path"
 
-sudo rm -r root/
+truncate -s 2G $image
+mkfs.ext4 $image
+sudo mount -oloop $image root/
 
-multistrap -a armel -d $PWD/root -f $release.conf && {
-	sudo cp -r /lib/firmware root/lib/
+[ -d root/lost+found ] && {
+	sudo rm -r root/lost+found
+} || {
+	punt
+}
+
+sudo multistrap -a armhf -d $PWD/root -f $release.conf && {
+	sudo cp -r $kern_path/firmware root/lib/ || cp_err
 	pushd $kern_path
-	make INSTALL_MOD_PATH=$deb_root modules_install
+	sudo make INSTALL_MOD_PATH=$deb_root modules_install || cp_err
 	popd
-	mkdir root/etc/init/
-	sudo cp .vimrc root/etc/nfinit/
-	sudo cp .screenrc root/etc/nfinit/
-	sudo cp .gitconfig root/etc/nfinit/
-	sudo cp .bashrc root/etc/nfinit/
-	sudo cp init_packages.sh root/bin/
-	sudo cp interfaces root/etc/network/
-	sudo echo "user ALL = NOPASSWD:ALL" >> root/etc/sudoers
-	sudo cp -f wpa.conf root/etc/dot_conf_examples
-	touch /tmp/multistrap_done
+	sudo mkdir -p root/etc/init/ || cp_err
+	sudo cp interfaces root/etc/network/ || cp_err
+	sudo bash -c 'echo 'LANG=en_US.UTF-8' >> root/etc/default/locale' || cp_err
+	sudo bash -c 'echo "user ALL = NOPASSWD:ALL" >> root/etc/sudoers' || cp_err
+	sudo cp -f wpa.conf root/etc/dot_conf_examples || cp_err
+	sudo mkdir -p root/home/init/ || cp_err
+	sudo mkdir root/zybot
+	sudo cp .vimrc root/zybot/
+	sudo cp .screenrc root/zybot/
+	sudo cp .gitconfig root/zybot/
+	sudo cp .bashrc root/zybot/
+	[ -f $cp_err_flag ] || touch $create_done
 }
 
-[ -f /tmp/multistrap_done ] || punt
+[ -f $create_done ] || punt
 
-sudo mount -t ext4 $sd_card $sd_mount/ && {
-	sudo rm -r $sd_mount/*
-	sudo cp -r root/* $sd_mount/
-	sudo chmod u+s $sd_mount/usr/bin/sudo
-}
+rm -rf $cp_err_flag
+rm -rf $create_done
 
-sleep 30
+for f in dev dev/pts sys proc run ; do sudo mount --bind /$f root/$f ; done
 
-#qemu stuff to init the rootfs
+sudo cp /usr/bin/qemu-arm-static root/usr/bin
+sudo cp init_packages.sh root/usr/bin
+cd root/
+sudo chroot . bin/bash
 
-sudo umount $sd_mount
+sleep 3
+
+for f in dev dev/pts sys proc run ; do sudo umount root/$f ; done
+
+#sudo umount -lf root/
+#cp -v $image $sandbox
